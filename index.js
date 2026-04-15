8a4a57236d6671c54a53970f15724de4e230aaa1
Aman dhoni <parleaman@gmail.com>
2026-04-15 16:09:34 +0530

    porject setup and ui

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const publicPath = path.resolve("./public");
+
+app.use(express.static(publicPath));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("list");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.get("/update", (req, res) => {
+  res.render("update");
+});
+
+app.post("/update", (req, res) => {
+  res.redirect("/");
+})
+
+app.post("/add", (req, res) => {
+  res.redirect("/");
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
